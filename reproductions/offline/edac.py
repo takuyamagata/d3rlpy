ef1ad4eac80a4d52ef15d5f82e4419d75ae8edaa
Lucia <lcipolina@gmail.com>
2023-07-10 11:02:45 +0100

    update reproductions files (IQL and EDAC)

--- reproductions/offline/edac.py
+++ reproductions/offline/edac.py
@@ -1,6 +1,7 @@
 import argparse
 import d3rlpy
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
@@ -9,9 +10,11 @@ def main():
     parser.add_argument('--n_critics', type=int, default=10)
     parser.add_argument('--eta', type=float, default=1.0)
     parser.add_argument('--gpu', type=int)
+    parser.add_argument('--mode', type=str, default='normal')
     args = parser.parse_args()
     
-    dataset, env = d3rlpy.datasets.get_dataset(args.dataset)
+    #dataset, env = d3rlpy.datasets.get_dataset(args.dataset)
+    dataset, env = d3rlpy.datasets.get_d4rl(args.dataset)
 
     # fix seed
     d3rlpy.seed(args.seed)
@@ -20,7 +23,15 @@ def main():
     _, test_episodes = train_test_split(dataset, test_size=0.2)
 
     encoder = d3rlpy.models.encoders.VectorEncoderFactory([256, 256, 256])
-    
+   
+    if args.mode == "delayed":
+        print("Delayed reward mode enabled")
+        for path in dataset.episodes:
+            total_return = np.sum(path.rewards)
+            for n in range(len(path.rewards)):
+                path.rewards[n] = 0
+            path.rewards[n] = total_return
+
     edac = d3rlpy.algos.EDAC(batch_size=256,
                            actor_learning_rate=3e-4,
                            critic_learning_rate=3e-4,
@@ -40,7 +51,8 @@ def main():
                 'environment': d3rlpy.metrics.evaluate_on_environment(env),
                 'value_scale': d3rlpy.metrics.average_value_estimation_scorer,
             },
-            experiment_name=f"EDAC_{args.dataset}_{args.seed}")
+            experiment_name=f"EDAC_{args.dataset}_eta{args.eta}_{args.seed}_{args.mode}",
+            with_timestamp=False,)
     
 if __name__ == '__main__':
     main()
--- reproductions/offline/iql.py
+++ reproductions/offline/iql.py
@@ -2,20 +2,30 @@ import argparse
 import d3rlpy
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from sklearn.model_selection import train_test_split
-
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='hopper-medium-v0')
     parser.add_argument('--seed', type=int, default=1)
     parser.add_argument('--gpu', type=int)
+    parser.add_argument('--mode', type=str, default='normal')
     args = parser.parse_args()
 
-    dataset, env = d3rlpy.datasets.get_dataset(args.dataset)
+    #dataset, env = d3rlpy.datasets.get_dataset(args.dataset)
+    dataset, env = d3rlpy.datasets.get_d4rl(args.dataset)
 
     # fix seed
     d3rlpy.seed(args.seed)
     env.seed(args.seed)
+
+    if args.mode == "delayed":
+        print("Delayed reward mode enabled")
+        for path in dataset.episodes:
+            total_return = np.sum(path.rewards)
+            for n in range(len(path.rewards)):
+                path.rewards[n] = 0
+            path.rewards[n] = total_return
 
     _, test_episodes = train_test_split(dataset, test_size=0.2)
 
@@ -48,7 +58,8 @@ def main():
                 'environment': d3rlpy.metrics.evaluate_on_environment(env),
                 'value_scale': d3rlpy.metrics.average_value_estimation_scorer,
             },
-            experiment_name=f"IQL_{args.dataset}_{args.seed}")
+            experiment_name=f"IQL_{args.dataset}_{args.seed}_{args.mode}",
+            with_timestamp=False,)
 
 
 if __name__ == '__main__':
