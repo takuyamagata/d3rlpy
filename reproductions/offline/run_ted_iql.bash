67928fe9909ed2047bb0c4cd1b595fd9ceeae68e
Taku Yamagata <taku.yamagata@bristol.ac.uk>
2024-10-04 15:44:25 +0100

    gpu option

--- reproductions/offline/run_ted_iql.bash
+++ reproductions/offline/run_ted_iql.bash
@@ -5,9 +5,10 @@ n_trials=100
 expectile=0.7
 weight_temp=3.0
 reward_scale=1000
+gpu=1
 for seed in 111 222 # 123 231 312
 do
-    python ted_iql.py --dataset $dataset --expectile $expectile --weight_temp $weight_temp --reward_scale $reward_scale --n_trials $n_trials
+    python ted_iql.py --dataset $dataset --expectile $expectile --weight_temp $weight_temp --reward_scale $reward_scale --n_trials $n_trials --seed $seed  --gpu $gpu
 done
 
 n_trials=100
@@ -16,5 +17,5 @@ weight_temp=10.0
 reward_scale=1000
 for seed in 111 222 # 123 231 312
 do
-    python ted_iql.py --dataset $dataset --expectile $expectile --weight_temp $weight_temp --reward_scale $reward_scale --n_trials $n_trials
+    python ted_iql.py --dataset $dataset --expectile $expectile --weight_temp $weight_temp --reward_scale $reward_scale --n_trials $n_trials --seed $seed  --gpu $gpu
 done
