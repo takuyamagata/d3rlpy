9d5f33a15837da4f2b150a91826b180f518488bd
Taku Yamagata <taku.yamagata@bristol.ac.uk>
2025-03-26 17:15:14 +0000

    stop share_encoder

--- d3rlpy/algos/qlearning/seq_iql.py
+++ d3rlpy/algos/qlearning/seq_iql.py
@@ -123,8 +123,8 @@ class SeqIQL(QLearningAlgoBase[SeqIQLImpl, SeqIQLConfig]):
             observation_shape,
             action_size,
             self._config.critic_encoder_factory,
-            # MeanQFunctionFactory(),
-            MeanQFunctionFactory(share_encoder=True), # default False
+            MeanQFunctionFactory(),
+            # MeanQFunctionFactory(share_encoder=True), # default False
             taylor_order=self._config.taylor_order,
             device=self._device,
             enable_ddp=self._enable_ddp,
